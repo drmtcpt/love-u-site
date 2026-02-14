e4fb1bdc395839e23da700cc209925b0b0ac6ed9
Creative Mango <crtvmng@gmail.com>
2026-02-14 18:35:53 +0400

    fix: suppress empty interface and require() linting errors

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -21,9 +21,8 @@ const Command = React.forwardRef<
 ));
 Command.displayName = CommandPrimitive.displayName;
 
-interface CommandDialogProps extends DialogProps {
-  // Dialog props
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface CommandDialogProps extends DialogProps {}
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
